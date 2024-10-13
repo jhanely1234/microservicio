e06c1988e78c95f7c8b61541e817ab1ecdd1a0c6
renykbzs9919 <lion.123967@gmail.com>
2024-10-13 12:43:48 -0400

    microservicio con whatsaap

---
+++ SERVER_URL_WHATSAPP/src/infrastructure/repositories/mock.repository.ts
@@ -0,0 +1,20 @@
+import { Lead } from "../../domain/lead";
+import LeadRepository from "../../domain/lead.repository";
+import { create } from 'venom-bot';
+
+class MockRepository implements LeadRepository {
+  getDetail(id: string): Promise<Lead | null | undefined> {
+      throw new Error("Method not implemented.");
+  }
+  save(): Promise<Lead> {
+   
+    const MOCK_LEAD: Lead = {
+      uuid: "00---000",
+      message: "test",
+      phone: "00000",
+    };
+    return Promise.resolve(MOCK_LEAD);
+  }
+}
+
+export default MockRepository
